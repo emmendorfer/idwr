7c2c82a8be2f456ee2ddd4fda3993a53fdf21543
emmendorfer <76491117+emmendorfer@users.noreply.github.com>
2020-12-28 10:48:03 -0300

    Add files via upload

---
+++ idwr.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec  8 11:37:10 2020
+
+@author: Leonardo Ramos Emmendorfer
+"""
+from numpy import sqrt
+from numpy import zeros
+from numpy import array
+from numpy import asarray
+
+
+def idwr(x,y,z,xnew,ynew):
+    '''Computes the interpolation from input points using the IDWR method.'''
+
+# The IDWR interpolation method is proposed in:
+# L. R. Emmendorfer, G. P. Dimuro, A novel formulation for 
+# inverse distance weighting from weighted linear regression, in: 
+# Computational Science - ICCS 2020 - 20th International Conference, 
+# Amsterdam, The Netherlands, June 3-5, 2020, Proceedings, Part II, 
+# Vol. 12138 of Lecture Notes in Computer 684 Science, Springer, 2020, 
+# pp. 576-589. doi:10.1007/978-3-030-50417-5 43.
+# URL https://doi.org/10.1007/978-3-030-50417-5 43
+
+# Usage: idwr(x,y,z,xnew,ynew)
+
+# The input, known points, are given as three arrays (x,y,z)
+# The new locations are given in (xnew,ynew)
+    
+
+    n=len(x)  # number of data points (input)
+    m=len(xnew)  #number of data points (output)
+    
+    znew_idwr=zeros(m, dtype=float)
+    
+    for j in range(m):   # compute all new points 
+        dist=zeros(n,dtype=float)
+        for i in range(n):
+            v1=array([x[i],y[i]])
+            v2=array([xnew[j],ynew[j]])
+            dist[i]=sqrt(sum((v1-v2)**2))  #Euclidean distance in R**2           
+        w=dist**(-2)
+        s=1/sum(w)
+        ws=w*s  # Weights
+        sumz=sum(z)
+        den=n**2-sum(dist**(-2))*sum(dist**2)
+        znew_idw=sum(ws*z)
+        znew_idwr[j]=znew_idw+n*(sumz-n*znew_idw)/den       
+    return znew_idwr
+
+
+def idw(x,y,z,xnew,ynew):
+    
+    '''Computes the interpolation from input points using the IDW method.'''
+
+# Usage: idw(x,y,z,xnew,ynew)
+
+# The input, known points, are given as three arrays (x,y,z)
+# The new locations are given in (xnew,ynew)
+
+    n=len(x)  # number of data points (input)
+    m=len(xnew)  #number of data points (output)
+      
+    znew_idw=zeros(m, dtype=float)
+    
+    for j in range(m):   # compute all new points 
+        dist=zeros(n,dtype=float)
+        for i in range(n):
+            v1=array([x[i],y[i]])
+            v2=array([xnew[j],ynew[j]])
+            dist[i]=sqrt(sum((v1-v2)**2))  #Euclidean distance                         
+        w=dist**(-2) 
+        s=1/sum(w)
+        ws=w*s  # Weights
+        znew_idw[j]=sum(ws*z)
+    return znew_idw
+
+
---
+++ plot_datasets.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 17 14:14:04 2020
+
+@author: Leonardo Ramos Emmendorfer
+"""
+import pandas as pd
+from scipy.interpolate import LinearNDInterpolator
+from scipy.interpolate import NearestNDInterpolator
+from idwr import idw
+from idwr import idwr
+import numpy as np
+from scipy.interpolate import interp2d
+from scipy.interpolate import Rbf
+
+
+from scipy.interpolate import SmoothBivariateSpline
+
+
+
+#Plot maps for "Amazon"
+
+arqle=pd.read_csv("texas.csv",names=None,header=None)
+vec=arqle.to_numpy();
+
+x=vec[:,0]
+y=vec[:,1]
+z=vec[:,2]
+
+npt=50
+dx=(max(x)-min(x))/npt
+dy=(max(y)-min(y))/npt
+
+xnew = np.arange(min(x), max(x),dx)
+ynew = np.arange(min(y), max(y), dy)
+#znew = f(xnew, ynew)
+
+xx, yy = np.meshgrid(xnew, ynew)
+
+xx=xx.reshape(npt*npt,1)[:,0]
+yy=yy.reshape(npt*npt,1)[:,0]
+
+
+zidw=idw(x,y,z,xx,yy)
+zidwr=idwr(x,y,z,xx,yy)
+
+znew_idw=zidw.reshape(50,50)
+znew_idwr=zidwr.reshape(50,50)
+
+import seaborn as sns
+sns.heatmap(znew_idw)
+
+sns.heatmap(znew_idwr)
+
+
+rbfi = Rbf(x, y, z)  # radial basis function interpolator instance
+zrbf = rbfi(xx, yy)   # interpolated values]
+
+znew_rbf=zrbf.reshape(50,50)
+ax = sns.heatmap(znew_rbf)
+
+
+points=np.zeros((len(x),2))
+points[:,0]=x
+points[:,1]=y 
+bvsi=NearestNDInterpolator(points,z)
+znn=bvsi(xx,yy)
+          
+znew_nn=znn.reshape(50,50)     
+         
+ax = sns.heatmap(znew_nn)
+
+points=np.zeros((len(x),2))
+points[:,0]=x
+points[:,1]=y
+
+interplin = LinearNDInterpolator(points, z, fill_value=0)
+zlin=interplin(xx, yy)
+    
+znew_lin=zlin.reshape(50,50)     
+         
+sns.heatmap(znew_lin)
+
+fcubic = interp2d(x, y, z, kind='cubic')  #spline
+zcubic=fcubic(xnew, ynew)
+ 
+znew_cubic=zcubic.reshape(50,50)     
+sns.heatmap(znew_cubic)
+
+bvs=SmoothBivariateSpline(x,y,z)
+
+zbvs=bvs.ev(xnew,ynew)  
+       
+znew_bvs=zlin.reshape(50,50)     
+         
+sns.heatmap(znew_bvs) 
+
