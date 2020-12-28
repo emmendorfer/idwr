370015bd6afaf2579f22485511fe1d1dfd86f5cd
emmendorfer <76491117+emmendorfer@users.noreply.github.com>
2020-12-28 17:29:02 -0300

    Update plot_datasets.py

--- plot_datasets.py
+++ plot_datasets.py
@@ -12,13 +12,10 @@ from idwr import idwr
 import numpy as np
 from scipy.interpolate import interp2d
 from scipy.interpolate import Rbf
-
-
 from scipy.interpolate import SmoothBivariateSpline
+import seaborn as sns
 
-
-
-#Plot maps for "Amazon"
+#Plot maps for the Texas dataset
 
 arqle=pd.read_csv("texas.csv",names=None,header=None)
 vec=arqle.to_numpy();
@@ -33,64 +30,37 @@ dy=(max(y)-min(y))/npt
 
 xnew = np.arange(min(x), max(x),dx)
 ynew = np.arange(min(y), max(y), dy)
-#znew = f(xnew, ynew)
-
 xx, yy = np.meshgrid(xnew, ynew)
 
 xx=xx.reshape(npt*npt,1)[:,0]
 yy=yy.reshape(npt*npt,1)[:,0]
 
-
 zidw=idw(x,y,z,xx,yy)
 zidwr=idwr(x,y,z,xx,yy)
-
 znew_idw=zidw.reshape(50,50)
 znew_idwr=zidwr.reshape(50,50)
 
-import seaborn as sns
 sns.heatmap(znew_idw)
 
 sns.heatmap(znew_idwr)
 
-
 rbfi = Rbf(x, y, z)  # radial basis function interpolator instance
 zrbf = rbfi(xx, yy)   # interpolated values]
-
 znew_rbf=zrbf.reshape(50,50)
-ax = sns.heatmap(znew_rbf)
 
+sns.heatmap(znew_rbf)
 
 points=np.zeros((len(x),2))
 points[:,0]=x
 points[:,1]=y 
 bvsi=NearestNDInterpolator(points,z)
 znn=bvsi(xx,yy)
-          
 znew_nn=znn.reshape(50,50)     
          
-ax = sns.heatmap(znew_nn)
-
-points=np.zeros((len(x),2))
-points[:,0]=x
-points[:,1]=y
-
-interplin = LinearNDInterpolator(points, z, fill_value=0)
-zlin=interplin(xx, yy)
-    
-znew_lin=zlin.reshape(50,50)     
-         
-sns.heatmap(znew_lin)
-
-fcubic = interp2d(x, y, z, kind='cubic')  #spline
-zcubic=fcubic(xnew, ynew)
- 
-znew_cubic=zcubic.reshape(50,50)     
-sns.heatmap(znew_cubic)
+sns.heatmap(znew_nn)
 
 bvs=SmoothBivariateSpline(x,y,z)
-
 zbvs=bvs.ev(xnew,ynew)  
-       
 znew_bvs=zlin.reshape(50,50)     
          
 sns.heatmap(znew_bvs) 
