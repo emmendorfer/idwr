247a6317b9b0b025b07c43d7f7653c344060a050
emmendorfer <76491117+emmendorfer@users.noreply.github.com>
2020-12-28 17:22:51 -0300

    Update idwr.py

--- idwr.py
+++ idwr.py
@@ -19,8 +19,8 @@ def idwr(x,y,z,xnew,ynew):
 # Computational Science - ICCS 2020 - 20th International Conference, 
 # Amsterdam, The Netherlands, June 3-5, 2020, Proceedings, Part II, 
 # Vol. 12138 of Lecture Notes in Computer 684 Science, Springer, 2020, 
-# pp. 576-589. doi:10.1007/978-3-030-50417-5 43.
-# URL https://doi.org/10.1007/978-3-030-50417-5 43
+# pp. 576-589. doi:10.1007/978-3-030-50417-5 
+# URL https://doi.org/10.1007/978-3-030-50417-5 
 
 # Usage: idwr(x,y,z,xnew,ynew)
 
